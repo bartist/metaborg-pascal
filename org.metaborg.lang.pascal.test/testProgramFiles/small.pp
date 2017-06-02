43ebe53d43561bddaefba566491221625e968430
Bart Heemskerk <de.uil.met.een@gmail.com>
2017-06-02 15:06:54 +0200

    Initial work on static semantics (started with labels)

--- org.metaborg.lang.pascal.test/testProgramFiles/small.pp
+++ org.metaborg.lang.pascal.test/testProgramFiles/small.pp
@@ -1,4 +1,13 @@
 program dinges ();
+label
+	1;
+function g(x : integer) : integer;
+	begin
+		g := x
+	end;
+procedure f(x : integer);
+ 	begin
+	end;
 begin
   { Write 'Hello World' }
   writeln('Hello World')
