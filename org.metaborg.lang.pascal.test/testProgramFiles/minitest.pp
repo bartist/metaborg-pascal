887204bc3eecdbc568b41b92a3a710228ff460f2
Bart Heemskerk <de.uil.met.een@gmail.com>
2017-06-20 12:18:41 +0200

    updated desugar to convert multiple with to nested with

--- org.metaborg.lang.pascal.test/testProgramFiles/minitest.pp
+++ org.metaborg.lang.pascal.test/testProgramFiles/minitest.pp
@@ -2,17 +2,42 @@ program test ();
   type 
   	intArray = array[1..5] of integer;
   	intMatrix = array[1..5] of array[1..5] of integer;
+	Pages = record
+	   page_nr: integer;
+	   content: string;
+	end;
+	Person = record
+	   name: string;
+	   surname: string;
+	   age: integer;
+	end;
+  	Books = record
+	   title: packed array [1..50] of char;
+	   author: Person;
+	   subject: packed array [1..100] of char;
+	   content: packed array [1..100] of Pages;
+	   book_id: integer;
+	end;
   var
     arr : intArray;
     intmat : intMatrix;
     i : integer;
     res : string;
+    book1: Books;
+    page: Pages;
+    me: Person;
   function f(x:string): string;
 	  begin
 	  	f := 'Test '' 123'
 	  end;
   begin
+{   with book1, page, me do
+   begin
+   	book_id := 1;
+   end; }
+   
    arr := [1,2,3,4,5];
+   (*arr[2] := book1.book_id;*)
    intmat[4] := arr;
    i := arr[3];
    res := f('dinges');
