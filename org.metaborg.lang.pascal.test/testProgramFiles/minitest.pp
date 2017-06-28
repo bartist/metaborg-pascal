302b987ee9d7d9f2e4f689ed3627c00fa22ced53
Bart Heemskerk <de.uil.met.een@gmail.com>
2017-06-28 16:11:24 +0200

    Implemented Enumeration Types

--- org.metaborg.lang.pascal.test/testProgramFiles/minitest.pp
+++ org.metaborg.lang.pascal.test/testProgramFiles/minitest.pp
@@ -1,48 +1,31 @@
 program test ();
   type 
-  	intArray = array[1..5] of integer;
-  	intMatrix = array[1..5] of array[1..5] of integer;
-	Pages = record
-	   page_nr: integer;
-	   content: string;
+	colors = (red, yellow, green, blue, tartan);
+	colorCode = array[red..tartan] of integer;
+	Date = record
+		day: integer;
+		month: integer;
+		year: integer;
 	end;
 	Person = record
 	   name: string;
 	   surname: string;
 	   age: integer;
-	end;
-  	Books = record
-	   title: packed array [1..50] of char;
-	   author: Person;
-	   subject: packed array [1..100] of char;
-	   content: packed array [1..100] of Pages;
-	   book_id: integer;
+	   birthdate : Date;
+	   eyes: colors;
 	end;
   var
-    arr : intArray;
-    intmat : intMatrix;
-    i : integer;
-    res : string;
-    book1: Books;
-    page: Pages;
     me: Person;
-  function f(x:string): string;
-	  begin
-	  	f := 'Test '' 123'
-	  end;
+    c : colors;
+    cc : colorCode;
   begin
-   i := 4;
-   with book1, author, me do
-   begin
-   	book_id := i;
-   end; 
-   
-   arr := [1,2,3,4,5];
-   arr[2] := book1.book_id;
-   intmat[4] := arr;
-   i := arr[3];
-   res := f('dinges');
-   if true then
-   	i := arr[4];   
-   writeln(res)
+	c := red;
+	cc[c] := 23;
+	with me, birthdate do
+	begin
+		eyes := green;
+		day := 16;
+		month := 5;
+		year := 1993;
+	end;
   end.
