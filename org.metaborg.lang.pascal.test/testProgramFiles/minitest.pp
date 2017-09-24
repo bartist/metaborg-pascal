59f9c0c92bf26596a15d43b648be00d548116fb2
Bart Heemskerk <de.uil.met.een@gmail.com>
2017-09-24 14:29:34 +0200

    final commit, cleanup, added Jenkinsfile

--- org.metaborg.lang.pascal.test/testProgramFiles/minitest.pp
+++ org.metaborg.lang.pascal.test/testProgramFiles/minitest.pp
@@ -6,19 +6,34 @@ program test ();
 	   name: string;
 	   surname: string;
 	   age: integer;
+	   other: Person;
 	   eyes: colors;
 	   case stuff : boolean of
 	   	true : (ja : string);
 	   	false : (nee : string)
 	end;
+	col = set of colors;
   var
+ 	x : col;
     me: Person;
+    notMe: Person;
     c : colors;
     cc : colorCode;
+    i : ^integer;
+    j : integer;
+    b : boolean;
+  function f (i : integer) : integer;
+  	begin
+  	  f := i;
+  	end;
   begin
 	c := red;
-	cc[c] := 23;
+	x := [red, blue];
+	b := blue in x;
+	j := 0;
+	cc[c] := f(j);
 	me.stuff := false;
+	me.other.eyes := green;
 	with me do
 	begin
 		eyes := green;
